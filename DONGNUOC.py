b4983e455e0add124382c36ca543cc03d4e5a2bc
Michael-Howard209z <nguyenhoang121314151617@gmail.com>
2025-10-28 07:45:52 +0000

    oleleolala

--- DONGNUOC.py
+++ DONGNUOC.py
@@ -4,7 +4,7 @@ with open("DONGNUOC.INP", "r") as f:
     N = int(f.read().strip())
 min_steps = float("inf")
 # Duyệt số lần đóng nước bằng cốc 5 từ lớn đến nhỏ
-for i in range(N // 5, -1, -1):
+for a in range(N // 5, -1, -1):
     remainder = N - 5 * a 
     if remainder % 3 == 0:
         b = remainder // 3 
@@ -15,6 +15,6 @@ for i in range(N // 5, -1, -1):
 # Ghi kết quả ra file DONGNUOC.OUT
 with open("DONGNUOC.OUT", "w") as f:
     if min_steps == float("inf"):
-        f.write("-l/n")
+        f.write("-1/n")
     else:
         f.write(str(min_steps)+ "\n")                
